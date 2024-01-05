375ba7124648e843c122a4fbeeadc2d10b160771
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-01-05 10:47:02 +0630

    add articles create, update and remove functions

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,17 @@
-export class CreateArticleDto {}
+// src/articles/dto/create-article.dto.ts
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateArticleDto {
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty({ required: false })
+  description?: string;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty({ required: false, default: false })
+  published?: boolean = false;
+}
