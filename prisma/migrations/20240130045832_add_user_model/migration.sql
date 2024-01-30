ed39ecbd11da74dfbb7271a45f1590e8e753b055
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-01-30 12:24:28 +0630

    add user model to database

---
+++ prisma/migrations/20240130045832_add_user_model/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE `Article` ADD COLUMN `authorId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Article` ADD CONSTRAINT `Article_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -24,4 +24,7 @@ export class ArticleEntity implements Article {
 
   @ApiProperty()
   updatedAt: Date;
+
+  @ApiProperty({ required: false, nullable: true })
+  authorId: number | null;
 }
